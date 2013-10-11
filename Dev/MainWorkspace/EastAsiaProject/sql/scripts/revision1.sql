354516e6ee3fcbc9d7885327920b42229716db29
brutalbarbarian <brutalbarbarian@gmail.com>
2013-10-12 09:56:14 +1300

    Update to script

--- Dev/MainWorkspace/EastAsiaProject/sql/scripts/revision1.sql
+++ Dev/MainWorkspace/EastAsiaProject/sql/scripts/revision1.sql
@@ -15,6 +15,13 @@ usr_id_created int,
 doc_date_changed datetime,
 usr_id_changed int,
 usr_id_owner int,
+doc_notes text,
+-- BOPersonalDocument fields
+doc_name varchar(50),
+-- BOChargeableDocument fields
+doc_stage int,
+doc_total double(16,2),
+
 constraint PK_DOC
 primary key(doc_id),
 
@@ -71,14 +78,129 @@ values
 	(1, 'Customer', 'CUS'),
 	(2, 'Work', 'WRK'),
 	(3, 'Invoice', 'INV'),
-	(4, 'School', 'SCH');
+	(4, 'School', 'SCH'),
+	(5, 'Company', 'COM');
 
-delete from TR_CDT_contact_details;
-delete from TM_INI_invoice_item;
-delete from TM_INV_invoice;
-delete from TM_WKI_work_item;
-delete from TM_WRK_work;
-delete from TR_STU_student;
-delete from TR_SCH_school;
-delete from TM_CUS_customer;
+-- clear all the tables
+drop table TR_CDT_contact_details;
+drop table TM_INI_invoice_item;
+drop table TM_INV_invoice;
+drop table TM_WKI_work_item;
+drop table TM_WRK_work;
+drop table TR_STU_student;
+drop table TR_SCH_school;
+drop table TM_CUS_customer;
+drop table TR_COM_company;
 
+create table TR_SCH_school (
+doc_id int,
+constraint PK_SCH
+primary key (doc_id),
+constraint FK_SCH_DOC
+foreign key (doc_id)
+references TM_DOC_document(doc_id)
+);
+
+create table TM_CUS_customer (
+doc_id int,
+cus_is_student boolean,
+constraint PK_CUS
+primary key (doc_id),
+constraint FK_CUS_DOC
+foreign key(doc_id)
+references TM_DOC_document(doc_id)
+);
+
+create table TM_STU_student(
+doc_id int,	-- customer
+doc_occurrence int,
+doc_id_school int,
+stu_date_start datetime,
+stu_date_end datetime,
+stu_notes text,
+constraint PK_STU
+primary key (doc_id, doc_occurrence),
+constraint FK_STU_CUS
+foreign key(doc_id)
+references TM_CUS_customer(doc_id),
+constraint FK_STU_SCH
+foreign key(doc_id_school)
+references TR_SCH_school(doc_id)
+);
+
+create table TR_COM_company(
+doc_id int,
+constraint PK_COM
+primary key (doc_id),
+constraint FK_COM_DOC
+foreign key (doc_id)
+references TM_DOC_document(doc_id)
+);
+
+create table TM_WRK_work(
+doc_id int,
+doc_id_customer int,
+doc_id_company int,
+wrk_date_required datetime,
+wrk_total_paid double(16,2),
+constraint PK_WRK
+primary key(doc_id),
+constraint FK_WRK_DOC
+foreign key (doc_id)
+references TM_DOC_document(doc_id),
+constraint FK_WRK_CUS
+foreign key (doc_id_customer)
+references TM_CUS_customer(doc_id),
+constraint FK_WRK_COM
+foreign key (doc_id_company)
+references TR_COM_company(doc_id)
+);
+
+create table TM_INV_invoice(
+doc_id int,
+doc_id_work int,
+constraint PK_INV
+primary key (doc_id),
+constraint FK_INV_DOC
+foreign key (doc_id)
+references TM_DOC_document(doc_id),
+constraint FK_INV_WRK
+foreign key (doc_id_work)
+references TM_WRK_work(doc_id)
+);
+
+create table TM_WKI_work_item(
+doc_id int,
+doc_occurrence int,
+prd_id int,
+wki_status int,
+wki_price double(16,2),
+wki_notes text,
+wki_quantity double,
+wki_quantity_invoiced double,
+constraint PK_WKI
+primary key(doc_id, doc_occurrence),
+constraint FK_WKI_WRK
+foreign key (doc_id)
+references TM_WRK_work(doc_id),
+constraint FK_WKI_PRD
+foreign key (prd_id)
+references TR_PRD_product(prd_id)
+);
+
+create table TM_INI_invoice_item(
+doc_id int,
+doc_occurrence int,
+doc_occurrence_source int,
+ini_price double(16,2),
+ini_quantity double,
+ini_notes text,
+constraint PK_INI
+primary key(doc_id, doc_occurrence),
+constraint FK_INI_INV
+foreign key (doc_id)
+references TM_INV_invoice(doc_id),
+constraint FK_INI_WKI
+foreign key (doc_id, doc_occurrence_source)
+references TM_WKI_work_item(doc_id, doc_occurrence)
+);
